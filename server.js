ef614f881c5537c8258215ec91be6e35f6a15b61
Vishvang <vishvangp.8@gmail.com>
2018-07-25 17:14:22 -0400

    Part 1 Usign express.js template pluging for handlebar. we can say we use Template Engine to load dynamic data to our template.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+// Load handlebars
+const hbs = require('hbs');
+
 var app = express();
 
+// Expres Middleware enable us to configure how your app works without configuring manually using
+// fucntion of exp object expresss.static inside middleware function app.us
+// export.static takes the absolute path to folder we want to serve 
+// __dirname store the path to our project direcotry 
 app.use(express.static(__dirname + '/public'));
 
+// tells express what engine we had like to use and we pass it in second arg
+app.set('view engine', 'hbs');
+
+// route configuration
 app.get('/',(req, res) => {
+    // render some html using express
     // res.send('<h1>Hello Express</h1>');
+
+    // res.send() going to respond back to req
     res.send({
         name: 'Vishvang',
         age:25,
@@ -13,6 +27,11 @@ app.get('/',(req, res) => {
     });
 });
 
+
+app.get('/about', (req, res) => {
+    //console.log('About page');
+    res.render('about.hbs');
+});
 // bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
     res.send({
@@ -20,4 +39,8 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+// By calling this we let app to start listening to req
+// here, we are using local port 3000
+app.listen(3000, ()=>{
+    console.log('server is up on to port 3000');
+});
