f1a7542a22d994d1512a8b9c8ecb0193bcb67a2b
Vishvang <vishvangp.8@gmail.com>
2018-07-25 14:05:09 -0400

    Adding default route parameter in code

--- server.js
+++ server.js
@@ -4,4 +4,20 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/',(req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    res.send({
+        name: 'Vishvang',
+        age:25,
+        likes: ['painting','cycling']
+    });
+});
+
+// bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'unable to handle request'
+    });
+});
+
 app.listen(3000);
